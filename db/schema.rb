2815b0f1b02768ed4dfe57305e58ab1cfca0c312
Paul Ruby <kolodiy6767@gmail.com>
2022-10-08 14:31:12 +0300

    chore (model): create Cart model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_090545) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_112941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "status", ["out_of_stock", "in_stock", "running_low"]
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -63,5 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_090545) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "categories"
 end
