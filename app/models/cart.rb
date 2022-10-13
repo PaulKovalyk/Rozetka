50c854a6e963f325978ecf820a857d7b7d56e053
Paul Ruby <kolodiy6767@gmail.com>
2022-10-13 18:54:41 +0300

    chore (cart.rb) : add validates user_id

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,16 +3,5 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :products
-
-  # def self.create_cart_for_user(user)
-  #     create(user_id: user.id)
-  # end
-
-  # def total_price
-  #     @total_price = 0
-  #     @cart.each do |product|
-  #         @total_price += product.price
-  #     end
-  #     @total_price
-  # end
+  validates :user_id, presence: true
 end
